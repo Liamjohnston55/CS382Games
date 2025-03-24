49fd9085ac58ad8834c2b7841cbcbc983b2d46b6
Liamjohnston55 <122326151+Liamjohnston55@users.noreply.github.com>
2025-03-24 15:39:26 -0500

    Got the pre-fab Models completed
    I added the spaceships and projectiles

---
+++ Project 3/Space SHUMP/Assets/Scripts/Modelsinfo.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Modelsinfo : MonoBehaviour
+{
+    // This is how I will display the model's information
+    [Header("I have downloaded this model from the following Website:")]
+    public string Website = "";
+}
